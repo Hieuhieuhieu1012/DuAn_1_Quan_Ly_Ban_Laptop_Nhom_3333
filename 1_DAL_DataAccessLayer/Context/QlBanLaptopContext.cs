378df05e7ed9e9127a1ffefb67edb135c0cc0e6a
Hieuuu <hieunmph19260@fpr.edu.vn>
2023-12-05 13:06:59 +0700

    ok

--- 1_DAL_DataAccessLayer/Context/QlBanLaptopContext.cs
+++ 1_DAL_DataAccessLayer/Context/QlBanLaptopContext.cs
@@ -50,7 +50,7 @@ namespace _1_DAL_DataAccessLayer.Context
             if (!optionsBuilder.IsConfigured)
             {
                 optionsBuilder.UseSqlServer(
-                    @"Data Source=DESKTOP-VGNG2FP\SQLEXPRESS;Initial Catalog=Du_An_1_Group_Done;Integrated Security=True");
+                    @"Data Source=DESKTOP-VGNG2FP\SQLEXPRESS;Initial Catalog=Du_An_1_Group_Test;Integrated Security=True");
             }
         }
 
